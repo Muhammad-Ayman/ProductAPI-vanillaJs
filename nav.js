41c0ee7a938561356daeccca476e0563ce8b8a45
Muhammad-Ayman <i.muhammadayman@gmail.com>
2024-09-07 17:57:32 +0300

    footer

--- nav.js
+++ nav.js
@@ -1,11 +1,11 @@
 let nav = document.querySelector("nav");
 
 nav.innerHTML = `<div class="logo">
-        <h4>Logo</h4>
+        <a href="./index.html"><h4>SHOP</h4></a>
       </div>
       <div class="links">
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="./index.html">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
